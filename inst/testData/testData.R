90c1e89843f4e51df2b79d4989c31eb6a790099c
Martin Aryee <aryee@jhu.edu>
2013-02-05 17:33:51 +0000

    Bugfix: Weights were previously ignored when smoothing beta
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/bumphunter@73174 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/testData/testData.R
+++ inst/testData/testData.R
@@ -3,30 +3,28 @@ library(bumphunter)
 ## Generate dummy data
 set.seed(123)
 dat <- dummyData()
-## matplot(dat$pos, dat$mat, type="l", col=c(rep("red", n1), rep("blue", n2)), lty=1)
-## save(dat, file = "../unitTests/dat.rda")
 
 ## loessByCluster      
 loessSmoothed <- loessByCluster(cluster=dat$cluster, y=dat$mat[,1],
                               bpSpan = 1000, minNum=7, minInSpan=5, maxSpan=1)
-save(loessSmoothed, file="../unitTests/loessSmoothed.rda")
+save(loessSmoothed, file="inst/unitTests/loessSmoothed.rda")
 
 ## runmedByCluster
 runmedSmoothed <- runmedByCluster(cluster=dat$cluster, y=dat$mat[,1],  k=5, endrule="constant")
-save(runmedSmoothed, file="../unitTests/runmedSmoothed.rda")
+save(runmedSmoothed, file="inst/unitTests/runmedSmoothed.rda")
 
 ## bumphunter test 1
 set.seed(123)
 bumpsTest1 <- bumphunter (dat$mat, design=dat$design, chr=dat$chr, pos=dat$pos, cluster=dat$cluster,
                           coef=2, cutoffQ=0.9, smooth=TRUE, B=500,
                           verbose=TRUE, smoothFunction=loessByCluster)
-save(bumpsTest1, file="../unitTests/bumpsTest1.rda")
+save(bumpsTest1, file="inst/unitTests/bumpsTest1.rda")
 
 ## bumphunter test 2: chr unspecified
 set.seed(123)
 bumpsTest2 <- bumphunter (dat$mat, design=dat$design, pos=dat$pos, cluster=dat$cluster,
                           coef=2, cutoffQ=0.9, maxGap=500, smooth=TRUE, B=500,
                           verbose=TRUE, smoothFunction=loessByCluster)
-save(bumpsTest2, file="../unitTests/bumpsTest2.rda")
+save(bumpsTest2, file="inst/unitTests/bumpsTest2.rda")
 
 
