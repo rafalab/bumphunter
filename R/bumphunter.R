bd6e781f4610ac75fb573244fb2a41e734646b43
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2013-04-11 18:05:43 +0000

    minor printout fix
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/bumphunter@75587 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bumphunter.R
+++ R/bumphunter.R
@@ -112,7 +112,7 @@ bumphunterEngine <- function(mat, design, chr=NULL, pos, cluster=NULL,
   
   if(is.null(cutoff))
     cutoff <- quantile(abs(permBeta), cutoffQ, na.rm=TRUE)
-  if(verbose) message(sprintf("bumphunterEngine cutoff: %s", round(cutoff,3)))
+  if(verbose) message(sprintf("bumphunterEngine: cutoff: %s", round(cutoff,3)))
   
   if(verbose) message("bumphunterEngine: Finding regions.")
   tab <- regionFinder(x=beta, chr=chr, pos=pos, cluster=cluster,
