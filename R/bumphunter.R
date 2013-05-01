84a9245e5146f3324ee3dc00b92685d250a7b8a4
khansen <khansen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-05-01 18:53:48 +0000

    some fixes so it now checks without error
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/bumphunter@76175 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bumphunter.R
+++ R/bumphunter.R
@@ -1,35 +1,49 @@
 setMethod("bumphunter", signature(object = "matrix"),
           function(object, design, chr=NULL, pos, cluster=NULL,
-                   coef=2, cutoff=NULL, pickCutoff=FALSE, cutoffQ=0.99,
+                   coef=2, cutoff=NULL, pickCutoff=FALSE, pickCutoffQ=0.99,
                    maxGap=500, smooth=FALSE, smoothFunction=locfitByCluster,
-                   useWeights=FALSE, B=1000, verbose=TRUE, ...){
+                   useWeights=FALSE, B=100, verbose=TRUE, ...){
             if(missing(design)) stop("design must be specified")
             if(missing(pos)) stop("If object is a matrix, pos must be specified")
-            bumphunterEngine(object, design=design, chr=chr, pos,
-                             cluster=cluster,
-                             coef=coef,cutoff=cutoff,pickCutoff=pickCutoff,
-                             cutoffQ=cutoffQ,
+            bumphunterEngine(object, design=design, chr=chr, pos, cluster=cluster,
+                             coef=coef,
+                             cutoff=cutoff, pickCutoff=pickCutoff, pickCutoffQ=pickCutoffQ,
                              maxGap=maxGap,smooth=smooth,
                              smoothFunction=smoothFunction,
-                             useWeights=useWeights,
-                             B=B, verbose=verbose, ...)
+                             useWeights=useWeights, B=B, verbose=verbose, ...)
           })
 
 bumphunterEngine <- function(mat, design, chr=NULL, pos, cluster=NULL,
-                             coef=2, cutoff=NULL, pickCutoff=FALSE,
-                             cutoffQ=0.99,
+                             coef=2,
+                             cutoff=NULL, pickCutoff=FALSE, pickCutoffQ=0.99, 
                              maxGap=500, smooth=FALSE,
-                             smoothFunction=loessByCluster,
-                             useWeights=FALSE, B=1000, verbose=TRUE, ...){
+                             smoothFunction=locfitByCluster,
+                             useWeights=FALSE, B=100, verbose=TRUE, ...){
+    ## cutoff = c(L, U)
+
     if(!is.matrix(mat))
         stop("'mat' must be a matrix.")
     if(ncol(mat) != nrow(design))
-        stop("Number of columns of data matrix must match number of rows of design matrix")
-    if(is.null(cutoff) & !pickCutoff) stop("Must pick a cutoff")
-    if(is.null(cutoff) & pickCutoff & B<1) stop("Must do at least one permution to pick a cutoff")
-    if(!is.null(cutoff) & pickCutoff)
-        warning("cutoff was supplied so ignoring pickCutoff=TRUE")
+        stop("Number of columns of 'mat' must  match number of rows of 'design'")
+    if(B < 0) stop("'B' has to be an integer greater or equal to zero")
+
+    if(! (is.null(cutoff) || length(cutoff) %in% 1:2))
+        stop("'cutoff' has to be either NULL or a vector of length 1 or 2")
+
+    if(length(cutoff) == 2)
+        cutoff <- sort(cutoff)
+
+    if(is.null(cutoff) && !pickCutoff)
+        stop("Must pick a cutoff, either using 'cutoff' or 'pickCutoff'")
+    if(!is.null(cutoff) && pickCutoff) {
+        pickCutoff <- FALSE
+        warning("'cutoff' was supplied so ignoring 'pickCutoff=TRUE'")
+    }
     
+    if(pickCutoff && (length(pickCutoffQ) != 1 || pickCutoff < 0 || pickCutoffQ > 1))
+        stop("Using `pickCutoff = TRUE' requires that 'pickCutoffQ' is a single number between 0 and 1")
+    if(pickCutoff && B<1) stop("Must do at least one permution to pick a cutoff")
+
     if (!getDoParRegistered())
         registerDoSEQ()
     workers <- getDoParWorkers()
@@ -54,24 +68,24 @@ bumphunterEngine <- function(mat, design, chr=NULL, pos, cluster=NULL,
     if(is.null(cluster))
         cluster <- clusterMaker(chr, pos, maxGap=maxGap)
     
-    if(B>0 & B< workers) stop("B must be bigger than workers (or 0)")
+    ## if(B>0 & B<workers) stop("B must be bigger than workers (or 0)")
     
     if(verbose) message("bumphunterEngine: Computing coefficients.")
     
     if(useWeights & smooth){
-        tmp <- .getEstimate(mat=mat, design=design, coef=coef, full=TRUE)
+        tmp <- bumphunter:::.getEstimate(mat=mat, design=design, coef=coef, full=TRUE)
         rawBeta <- tmp$coef
         weights <- tmp$sigma
         rm(tmp)
     } else{
-        rawBeta <- .getEstimate(mat=mat, design=design, coef=coef, full=FALSE)
+        rawBeta <- bumphunter:::.getEstimate(mat=mat, design=design, coef=coef, full=FALSE)
         weights <- NULL
     }
   
     if(smooth){
         if(verbose) message("bumphunterEngine: Smoothing coefficients.")
         beta <- smoother(y=rawBeta, x=pos, cluster=cluster, weights=weights,
-                         smoothFunction=smoothFunction, verbose=subverbose,...) 
+                         smoothFunction=smoothFunction, verbose=subverbose, ...) 
         Index <- which(beta$smoothed)
         beta <- beta$fitted
     } else {
@@ -82,12 +96,12 @@ bumphunterEngine <- function(mat, design, chr=NULL, pos, cluster=NULL,
     if(B>0){
         if (verbose) message("bumphunterEngine: Performing ", B, " permutations.")
         if(useWeights & smooth){
-            tmp <- .getEstimate(mat, design, coef, B, full=TRUE)
+            tmp <- bumphunter:::.getEstimate(mat, design, coef, B, full=TRUE)
             permRawBeta <- tmp$coef
             weights <- tmp$sigma
             rm(tmp)
         } else{
-            permRawBeta <- .getEstimate(mat, design, coef, B, full=FALSE)
+            permRawBeta <- bumphunter:::.getEstimate(mat, design, coef, B, full=FALSE)
             weights <- NULL
         }
         
@@ -109,7 +123,7 @@ bumphunterEngine <- function(mat, design, chr=NULL, pos, cluster=NULL,
         } else permBeta <- permRawBeta
         
         if(is.null(cutoff))
-            cutoff <- quantile(abs(permBeta), cutoffQ, na.rm=TRUE)
+            cutoff <- quantile(abs(permBeta), pickCutoffQ, na.rm=TRUE)
         if(verbose) message(sprintf("bumphunterEngine: cutoff: %s", round(cutoff,3)))
     } ## Done with permutations
     
@@ -127,14 +141,13 @@ bumphunterEngine <- function(mat, design, chr=NULL, pos, cluster=NULL,
     
     if (verbose) message("bumphunterEngine: Computing regions for each permutation.")
     chunksize <- ceiling(B/workers)
-    foreach_object <- foreach(subMat=iter(permBeta, by="col", chunksize=chunksize),
-                              .combine="c", .packages = "bumphunter")
-    foreach_argname <- foreach_object$argnames[1]
-    nulltabs <- foreach_object %dorng% { 
-	subMat <- get(foreach_argname)
-	apply(subMat, 2, regionFinder, chr=chr, pos=pos, cluster=cluster,
-              cutoff=cutoff, ind=Index, verbose=FALSE)
-    }
+    subMat <- NULL
+    nulltabs <- foreach(subMat=iter(permBeta, by="col", chunksize=chunksize),
+                        .combine="c", .packages = "bumphunter") %dorng% {
+                            ## What to do
+                            apply(subMat, 2, regionFinder, chr=chr, pos=pos, cluster=cluster,
+                                  cutoff=cutoff, ind=Index, verbose=FALSE)
+                        }
     attributes(nulltabs)[["rng"]] <- NULL
     
     if (verbose) message("bumphunterEngine: Estimating p-values and FWER.")
@@ -180,7 +193,13 @@ bumphunterEngine <- function(mat, design, chr=NULL, pos, cluster=NULL,
     tab$fwerArea <- rate2
     
     tab <- tab[order(tab$fwer,-tab$area),]
-    return(list(table=tab, coef=rawBeta, fitted=beta,
-                pvaluesMarginal=pvs, null=list(value=V,length=L)))
+
+    algorithm <- list(version = packageDescription("bumphunter")$Version, coef = coef,
+                     cutoff = cutoff, pickCutoff = pickCutoff, pickCutoffQ = pickCutoffQ,
+                     smooth = smooth, maxGap = maxGap, B = B, useWeights = useWeights,
+                     smoothFunction = deparse(substitute(smoothFunction)))
+    ret <- list(table=tab, coef=rawBeta, fitted=beta,
+                pvaluesMarginal=pvs, null=list(value=V,length=L), algorithm = algorithm)
+    return(ret)
 }
 
--- inst/unitTests/test_bumphunter.R
+++ inst/unitTests/test_bumphunter.R
@@ -6,18 +6,20 @@ test_bumphunter <- function () {
     ## Test 1
     set.seed(123)
     bumps <- bumphunter (dat$mat, design=dat$design, chr=dat$chr, pos=dat$pos,
-                         cluster=dat$cluster, coef=2, cutoffQ=0.9,
+                         cluster=dat$cluster, coef=2, cutoff=0.28,
                          smooth=TRUE, B=500, verbose=TRUE, smoothFunction=loessByCluster)
     load(file.path(path.package("bumphunter"), "unitTests", "bumpsTest1.rda"))
-    checkEquals(bumps, bumpsTest1)
+    nam <- setdiff(names(bumps), "algorithm")
+    checkEquals(bumps[nam], bumpsTest1[nam])
     
     ## Test 2: chr unspecified
     set.seed(123)
     bumps <- bumphunter (dat$mat, design=dat$design, pos=dat$pos,
-                         cluster=dat$cluster, coef=2, cutoffQ=0.9,
+                         cluster=dat$cluster, coef=2, cutoff=0.28,
                          maxGap=500, smooth=TRUE, B=500, verbose=TRUE, smoothFunction=loessByCluster)
     load(file.path(path.package("bumphunter"), "unitTests", "bumpsTest2.rda"))
-    checkEquals(bumps, bumpsTest2)
+    nam <- setdiff(names(bumps), "algorithm")
+    checkEquals(bumps[nam], bumpsTest2[nam])
 }
 
 test_bumphunterParallel <- function() {
@@ -28,18 +30,20 @@ test_bumphunterParallel <- function() {
         registerDoParallel(cores = 2)
         set.seed(123)
         bumps <- bumphunter (dat$mat, design=dat$design, chr=dat$chr, pos=dat$pos,
-                             cluster=dat$cluster, coef=2, cutoffQ=0.9,
+                             cluster=dat$cluster, coef=2, cutoff=0.28,
                              smooth=TRUE, B=500, verbose=TRUE, smoothFunction=loessByCluster)
         load(file.path(path.package("bumphunter"), "unitTests", "bumpsTest1.rda"))
-        checkEquals(bumps, bumpsTest1)
+        nam <- setdiff(names(bumps), "algorithm")
+        checkEquals(bumps[nam], bumpsTest1[nam])
         cl <- makeCluster(2)
         registerDoParallel(cl = cl)
         set.seed(123)
         bumps <- bumphunter (dat$mat, design=dat$design, chr=dat$chr, pos=dat$pos,
-                             cluster=dat$cluster, coef=2, cutoffQ=0.9,
+                             cluster=dat$cluster, coef=2, cutoff=0.28,
                              smooth=TRUE, B=500, verbose=TRUE, smoothFunction=loessByCluster)
         load(file.path(path.package("bumphunter"), "unitTests", "bumpsTest1.rda"))
-        checkEquals(bumps, bumpsTest1)
+        nam <- setdiff(names(bumps), "algorithm")
+        checkEquals(bumps[nam], bumpsTest1[nam])
     }
 }
     
