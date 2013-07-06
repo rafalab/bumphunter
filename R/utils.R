2dcbaf9ac20f7157dbfa70f7b4747ca9f043a697
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2013-07-06 00:32:47 +0000

    added closeSockets
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/bumphunter@78136 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils.R
+++ R/utils.R
@@ -2,4 +2,9 @@ greaterOrEqual <- function(x,y) {
     precision <- sqrt(.Machine$double.eps)
     (x >= y) | (abs(x-y) <= precision)
 }
-    
+
+closeSockets <- function() {
+    allCon <- showConnections()
+    socketCon <- as.integer(rownames(allCon)[allCon[, "class"] == "sockconn"])
+    sapply(socketCon, function(ii) close.connection(getConnection(ii)) )
+}
